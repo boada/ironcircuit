860048d661991b2b957278fd0a9415cc401021e1
Steven <stevenboada@gmail.com>
2017-08-08 22:15:54 -0400

    initial commit
    started thinking about how to actual code it

---
+++ seasons.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Top level docstring that I'll write later.
+
+'''
+
+import pandas
+import numpy
+
+__version__ = '0.01'
+__author__ = 'Steven Boada'
+
+class season():
+    '''
+    Some docstring that I will write later.abs
+
+    '''
+
+    def __init__(self):
+        pass
+
+    def tallyScore(self):
+        pass
+
+    def schedule(self):
+        pass
+
+    def writeMarkdown(self):
+        pass
+
+    def createSeason(self):
+        pass
+
+    def endSeason(self):
+        pass
+
+    def handleFiles(self):
+        pass
+
+    def build(self):
+        pass
+
+    def publish(self):
+        pass
+
+if __name__ == "__main__":
+    pass
